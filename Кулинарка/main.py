d8abe701ba8e26566659857cc677a70b06dc6a58
Similar3101 <alecsander.mac@gmail.com>
2022-03-01 16:36:20 +0800

    Создание кода

---
+++ Кулинарка/main.py
@@ -0,0 +1,46 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import random
+# from pprint import pprint
+
+
+TOKEN = '1960805a354085e3eff3b7604706f98fb505669be7f68e3564080a4b19c5efb032c8de2530676593c891f'
+
+
+def send_mes(vk, chat, text):
+    random_id = random.randint(0, 2 ** 64)
+    vk.method('messages.send', {'chat_id': chat, 'message': text, 'random_id': random_id})
+
+
+def main():
+    vk_session = vk_api.VkApi(
+        token=TOKEN)
+
+    # longpoll = VkBotLongPoll(vk_session, 210739018, wait=1)
+    longpoll = VkLongPoll(vk_session)
+    print('работаем')
+    for event in longpoll.listen():
+        print(event.type)
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
+                if event.from_chat:
+                    print(event.type)
+                    print('Новое сообщение:')
+                    print('Для меня от:', event.chat_id)
+                    print('Текст:', event.text)
+                    send_mes(vk_session, event.chat_id, event.text)
+        if event.type == VkEventType.CHAT_UPDATE:
+            text = '''Приветствую вас! Я Бот Кулинарная книга.
+Я смогу (когда-нибудь) написать определённый рецепт блюда, 
+написать список блюд, у которых есть рецепт в книге, 
+добавлять в список новые блюда и прочие действия.'''
+            send_mes(vk_session, event.chat_id, text)
+            # vk = vk_session.get_api()
+            # vk.messages.send(user_id=event.obj.message['from_id'],
+            #                  message="Спасибо, что написали нам. Мы обязательно ответим",
+            #                  random_id=random.randint(0, 2 ** 64))
+            # pprint(vars(event))
+
+
+if __name__ == '__main__':
+    main()
