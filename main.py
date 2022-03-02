69d9def4b25f5b3b9ee5657b09d66fa769cda9f8
Similar3101 <alecsander.mac@gmail.com>
2022-03-02 16:51:11 +0800

    дщд

---
+++ main.py
@@ -0,0 +1,31 @@
+# код
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/training/<prof>')
+def train(prof):
+    if 'строитель' in prof or 'инженер' in prof:
+        img = 'sci.png'
+        name_of_picture = 'Инженерные тренажёры'
+    else:
+        name_of_picture = 'Научные симуляторы'
+        img = 'ing.png'
+    return render_template('content.html', title=prof, img=img, name_of_picture=name_of_picture)
+
+
+@app.route('/index')
+def lol():
+    return render_template('content.html', title='title', img='../picture/ing.png', name_of_picture='lol')
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
