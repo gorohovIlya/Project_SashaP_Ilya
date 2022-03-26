076a16551fe73b59a162fb1244a2607271711508
Similar3101 <93250648+Similar3101@users.noreply.github.com>
2022-03-26 12:03:49 +0800

    Update randommeal.py
    сделал функцию случайноеблюдо

--- Кулинарка/randommeal.py
+++ Кулинарка/randommeal.py
@@ -1,2 +1,18 @@
+from random import choice
+import sqlite3
+from howtoprepare import howtoprepare
+
+
 def randommeal():
-    return "команда в разработке "
+    connection = sqlite3.connect('cooking.db')
+    cursor = connection.cursor()
+    # meals = list(map(lambda x: ''.join(x), cursor.execute('SELECT meal_type FROM meal_types').fetchall()))
+    # meal = choice(meals)
+    meal = 'каша'
+    mtype = cursor.execute(f'SELECT meals FROM meal_types WHERE meal_type = "{meal}"').fetchall()
+    mtype = choice(mtype[0][0].split(', '))
+    return f"{meal} {mtype}\n{howtoprepare(' '.join([meal, mtype]))}"
+
+
+if __name__ == '__main__':
+    print(randommeal())
