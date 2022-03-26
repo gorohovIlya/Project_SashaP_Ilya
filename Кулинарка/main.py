ca262268bf79f002a4d9da955564d048a7e429ff
Similar3101 <93250648+Similar3101@users.noreply.github.com>
2022-03-26 14:20:14 +0800

    Update main.py
    небольшое изменение

--- Кулинарка/main.py
+++ Кулинарка/main.py
@@ -1,12 +1,10 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import random
-import sqlite3
 from whattocookfrom import whattocookfrom
 from howtoprepare import howtoprepare
 from randommeal import randommeal
 from help import help
-# from pprint import pprint
 
 
 TOKEN = '1960805a354085e3eff3b7604706f98fb505669be7f68e3564080a4b19c5efb032c8de2530676593c891f'
@@ -21,7 +19,6 @@ def main():
     vk_session = vk_api.VkApi(
         token=TOKEN)
 
-    # longpoll = VkBotLongPoll(vk_session, 210739018, wait=1)
     longpoll = VkLongPoll(vk_session)
     print('работаем')
     for event in longpoll.listen():
@@ -33,12 +30,13 @@ def main():
                     print('Новое сообщение:')
                     print('Для меня от:', event.chat_id)
                     print('Текст:', event.text)
-                    # send_mes(vk_session, event.chat_id, event.text)
-                    if event.text[0:14] == "какприготовить":
-                        send_mes(vk_session, event.chat_id, howtoprepare(event.text[14:]))
-                    elif event.text == "чтоприготовитьиз":
-                        send_mes(vk_session, event.chat_id, whattocookfrom(event.text))
-                    elif event.text == "случайноеблюдо":
+                    if event.text[0:15] == "?какприготовить":
+                        print(event.text[15:])
+                        send_mes(vk_session, event.chat_id, howtoprepare(event.text[16:]))
+                    elif event.text[0:17] == "?чтоприготовитьиз":
+                        print(event.text[17:])
+                        send_mes(vk_session, event.chat_id, whattocookfrom(event.text[18:]))
+                    elif event.text == "?случайноеблюдо":
                         send_mes(vk_session, event.chat_id, randommeal())
                     elif event.text == "помощь":
                         send_mes(vk_session, event.chat_id, help())
@@ -48,11 +46,6 @@ def main():
 написать список блюд, у которых есть рецепт в книге,
 добавлять в список новые блюда и прочие действия.'''
             send_mes(vk_session, event.chat_id, text)
-            # vk = vk_session.get_api()
-            # vk.messages.send(user_id=event.obj.message['from_id'],
-            #                  message="Спасибо, что написали нам. Мы обязательно ответим",
-            #                  random_id=random.randint(0, 2 ** 64))
-            # pprint(vars(event))
 
 
 if __name__ == '__main__':
